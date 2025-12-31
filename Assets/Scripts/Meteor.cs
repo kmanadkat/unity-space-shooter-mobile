28a0afcc91ca1770f777d8aef99ca22c5a1c9422
kmanadkat <kmanadkat@gmail.com>
2025-12-31 19:51:38 +0530

    feat: Meteor Rotates While Falling

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -6,12 +6,16 @@ public class Meteor : Enemy {
     [SerializeField] private float maxSpeed;
     private float speed;
 
+    [SerializeField] private float zRotateSpeed;
+
     void Start() {
         speed = Random.Range(minSpeed, maxSpeed);
         rigidBody.linearVelocity = Vector2.down * speed;
     }
 
-    void Update() { }
+    void Update() {
+        transform.Rotate(0, 0, zRotateSpeed * Time.deltaTime);
+    }
 
     public override void HurtSequence() {
         // TODO
