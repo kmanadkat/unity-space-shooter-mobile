a278d1377391d2db84bea20c01c01b7344a578b8
kmanadkat <kmanadkat@gmail.com>
2025-12-31 18:27:46 +0530

    feat: Inheritance Recap

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] protected float health;
+
+    void Start() { }
+
+    void Update() { }
+
+    public void TakeDamage(float damage) {
+        health -= damage;
+        HurtSequence();
+
+        // Destroy Enemy
+        if (health <= 0) {
+            DeathSequence();
+        }
+    }
+
+    public virtual void HurtSequence() {
+        // TODO: Damage Animation
+    }
+
+    public virtual void DeathSequence() {
+        // TODO: Destroy Animation
+    }
+}
---
+++ Assets/Scripts/Meteor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Meteor : Enemy {
+    void Start() { }
+
+    void Update() { }
+
+    public override void HurtSequence() {
+        // TODO
+        // base.HurtSequence();
+    }
+
+    public override void DeathSequence() {
+        // TODO
+        // base.DeathSequence();
+    }
+}
