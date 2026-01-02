6d21441a527091ce11251c942ea42fb8b216099f
kmanadkat <kmanadkat@gmail.com>
2026-01-02 16:50:53 +0530

    feat: Player Health Bar

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
 public class PlayerStats : MonoBehaviour {
     [SerializeField] private float maxHealth;
     private float currentHealth;
 
+    // UI Health Bar Fill
+    [SerializeField] private Image healthFill;
+
     void Start() {
         currentHealth = maxHealth;
+        healthFill.fillAmount = currentHealth / maxHealth;
     }
 
     public void PlayerTakeDamage(float damage) {
         currentHealth -= damage;
+        healthFill.fillAmount = currentHealth / maxHealth;
         if (currentHealth <= 0) {
             Destroy(gameObject);
         }
