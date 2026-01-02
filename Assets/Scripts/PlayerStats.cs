e856888bf4ff0cbab35f4c5970d64ea3d33cfd4e
kmanadkat <kmanadkat@gmail.com>
2026-01-02 17:58:44 +0530

    feat: Player Damage Animation

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using Image = UnityEngine.UI.Image;
 
@@ -9,6 +10,9 @@ public class PlayerStats : MonoBehaviour {
     [SerializeField] private Image healthFill;
     [SerializeField] private GameObject explosionPrefab;
 
+    [SerializeField] private Animator anim;
+    private bool canPlayAnim = true;
+
     void Start() {
         currentHealth = maxHealth;
         healthFill.fillAmount = currentHealth / maxHealth;
@@ -17,9 +21,21 @@ public class PlayerStats : MonoBehaviour {
     public void PlayerTakeDamage(float damage) {
         currentHealth -= damage;
         healthFill.fillAmount = currentHealth / maxHealth;
+
+        // Play Animation
+        if (canPlayAnim) {
+            anim.SetTrigger("Damage");
+            StartCoroutine(AntiSpamAnimation());
+        }
         if (currentHealth <= 0) {
             Instantiate(explosionPrefab, transform.position, transform.rotation);
             Destroy(gameObject);
         }
+    }
+
+    private IEnumerator AntiSpamAnimation() {
+        canPlayAnim = false;
+        yield return new WaitForSeconds(0.15f);
+        canPlayAnim = true;
     }
 }
